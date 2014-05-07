b11373cc8d17063cbe280c14440a9ce5507f3b27
ratman <ratman@avast.com>
2014-05-07 17:51:16 +0200

    Code cleaning

--- Main/Api/Google/Geocode.php
+++ Main/Api/Google/Geocode.php
@@ -16,175 +16,165 @@ defined("_APP_ACCESS") or die(header("HTTP/1.0 404 Not Found"));
 
 class Main_Api_Google_Geocode
 {
-    /**
-	 * @var $apiRestUrl
-	 */
-	private $apiRestUrl = 'http://maps.google.com/maps/api/geocode/json';
+  /**
+   * @var $apiRestUrl
+   */
+  private $apiRestUrl = 'http://maps.google.com/maps/api/geocode/json';
 
-    /**
-	 * @var $urlParams
-	 */
-	private $urlParams = array(
-								'sensor' 	=> 'false',
-								'address'	=> ''
-							);
+  /**
+   * @var $urlParams
+   */
+  private $urlParams = array(
+    'sensor' 	=> 'false',
+    'address'	=> ''
+  );
 
-    /**
-	 * @var $json
-	 */
-	private $json = null;
+  /**
+   * @var $json
+   */
+  private $json = null;
 
-    /**
-	 * @var $result
-	 */
-	private $result = null;
+  /**
+   * @var $result
+   */
+  private $result = null;
 
-
-    /**
+  /**
     * Class constructor
     * @method __construct
     * @param string $address - address to check
     * @param string $language - language of responded data
     */
-    public function __construct( $address = '', $language = 'en' )
-    {
-    	$addressValidator = new Zend_Validate_Regex(array('pattern' => '/[a-zA-Z0-9\s\-\_\,]+/'));
-    	$languageValidator = new Zend_Validate_Regex(array('pattern' => '/[a-z]{,3}/'));
+  public function __construct( $address = '', $language = 'en' ){
+    $addressValidator = new Zend_Validate_Regex(array('pattern' => '/[a-zA-Z0-9\s\-\_\,]+/'));
+    $languageValidator = new Zend_Validate_Regex(array('pattern' => '/[a-z]{,3}/'));
 
 		if ( !$languageValidator->isValid( $language )) {
-			$this->urlParams['language'] = $language;
-		}
-
-        if ($addressValidator->isValid( $address )) {
-	    	$this->urlParams['address'] = $address;
-	    	
-			$query = $this->apiRestUrl . '?' . http_build_query($this->urlParams);
-			//debug($query);
-
-	    	$this->json = @file_get_contents( $query );
-	    	$decoded = json_decode($this->json);
-	    	
-	    	if( isset($decoded->status) && $decoded->status !== 'ZERO_RESULTS' ){
-	    		$this->result = $decoded->results[0];
-	    	}
-		}
+      $this->urlParams['language'] = $language;
     }
 
-    /**
+    if ($addressValidator->isValid( $address )) {
+      $this->urlParams['address'] = $address;
+
+      $query = $this->apiRestUrl . '?' . http_build_query($this->urlParams);
+      $this->json = @file_get_contents( $query );
+      $decoded = json_decode($this->json);
+
+      if( isset($decoded->status) && $decoded->status !== 'ZERO_RESULTS' ){
+        $this->result = $decoded->results[0];
+      }
+    }
+  }
+
+  /**
     * Method will get full response JSON object
     * @method getResultJson
     * @return float
     */
-    public function getResultJson(){
-    	return $this->json;
-	}
+  public function getResultJson(){
+    return $this->json;
+  }
 
     /**
     * Method will get lattitude from response
     * @method getLattitude
     * @return float
     */
-    public function getLattitude(){
-    	return isset($this->result->geometry->location->lat) ? $this->result->geometry->location->lat : null;
-	}
+  public function getLattitude(){
+   	return isset($this->result->geometry->location->lat) ? $this->result->geometry->location->lat : null;
+  }
 
-    /**
+  /**
     * Method will get longitude from response
     * @method getLongitude
     * @return float
     */
-    public function getLongitude(){
-    	return isset($this->result->geometry->location->lng) ? $this->result->geometry->location->lng : null;
-	}
+  public function getLongitude(){
+   	return isset($this->result->geometry->location->lng) ? $this->result->geometry->location->lng : null;
+  }
 
-    /**
+  /**
     * Method will get street long name
     * @method getStreetLongName
     * @return string
     */
-    public function getStreetLongName(){
-    	foreach( $this->result->address_components as $a ){
-    		if( $a->types[0] === 'route' ){
-    			return $a->long_name;
-    		}
-    	}
+  public function getStreetLongName(){
+  	foreach( $this->result->address_components as $a ){
+  		if( $a->types[0] === 'route' ){
+  			return $a->long_name;
+      }
+    }
+  	return null;
+  }
 
-    	return null;
-	}
-
-    /**
+  /**
     * Method will get street short name
     * @method getStreetShortName
     * @return string
     */
-    public function getStreetShortName(){
-    	foreach( $this->result->address_components as $a ){
-    		if( $a->types[0] === 'route' ){
-    			return $a->short_name;
-    		}
-    	}
-    	
-    	return null;
-	}
+  public function getStreetShortName(){
+    foreach( $this->result->address_components as $a ){
+    	if( $a->types[0] === 'route' ){
+    		return $a->short_name;
+      }
+    }
+    return null;
+  }
 
-    /**
+  /**
     * Method will get streen short name
     * @method getSublocalityLongName
     * @return string
     */
-    public function getSublocalityLongName(){
-    	foreach( $this->result->address_components as $a ){
-    		if( $a->types[0] === 'sublocality' ){
-    			return $a->long_name;
-    		}
-    	}
-    	
-    	return null;
+  public function getSublocalityLongName(){
+  	foreach( $this->result->address_components as $a ){
+  		if( $a->types[0] === 'sublocality' ){
+  			return $a->long_name;
+      }
+    }
+   	return null;
 	}
 
-    /**
+  /**
     * Method will get streen short name
     * @method getSublocalityShortName
     * @return string
     */
-    public function getSublocalityShortName(){
-    	foreach( $this->result->address_components as $a ){
-    		if( $a->types[0] === 'sublocality' ){
-    			return $a->short_name;
-    		}
-    	}
-    	
-    	return null;
+  public function getSublocalityShortName(){
+    foreach( $this->result->address_components as $a ){
+    	if( $a->types[0] === 'sublocality' ){
+    		return $a->short_name;
+      }
+    }
+    return null;
 	}
 
-    /**
+  /**
     * Method will get streen short name
     * @method getLocalityLongName
     * @return string
     */
-    public function getLocalityLongName(){
-    	foreach( $this->result->address_components as $a ){
-    		if( $a->types[0] === 'locality' ){
-    			return $a->long_name;
-    		}
-    	}
-    	
-    	return null;
+  public function getLocalityLongName(){
+    foreach( $this->result->address_components as $a ){
+    	if( $a->types[0] === 'locality' ){
+    		return $a->long_name;
+      }
+    }
+    return null;
 	}
 
-    /**
+  /**
     * Method will get streen short name
     * @method getLocalityShortName
     * @return string
     */
-    public function getLocalityShortName(){
-    	foreach( $this->result->address_components as $a ){
-    		if( $a->types[0] === 'locality' ){
-    			return $a->short_name;
-    		}
-    	}
-    	
-    	return null;
-	}
+  public function getLocalityShortName(){
+    foreach( $this->result->address_components as $a ){
+    	if( $a->types[0] === 'locality' ){
+    		return $a->short_name;
+      }
+    }
+    return null;
+  }
 
 }
